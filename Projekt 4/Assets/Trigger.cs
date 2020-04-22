b908ff772c6f50fd6022a5e31f10cf67c8d47e56
LeeZer0 <37550523+LeeZer0@users.noreply.github.com>
2020-04-22 12:39:38 +0200

    xxx

---
+++ Projekt 4/Assets/Trigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerStay(Collider col)
+    {
+        
+        if (col.gameObject.name == "Pocisk(Clone)")
+        {
+            Debug.Log("kolor");
+            rend.material.SetColor("_Color", Random.ColorHSV());
+
+        }
+    }
+}
