63dba426dbf157c5af544b838bdd4b817c25016a
LeeZer0 <37550523+LeeZer0@users.noreply.github.com>
2020-04-26 14:55:19 +0200

    6

---
+++ Projekt 6/Assets/Animacja.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Numerics;
+using UnityEngine;
+
+public class Animacja : MonoBehaviour
+{
+    GameObject player;
+    public Animator anim;
+    private bool klik;
+
+
+
+    void Start()
+    {
+        anim = player.GetComponent<Animator>();
+        klik = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.T) && klik==false)
+        {
+            anim.SetBool("machaj", true);
+            klik = true;
+            Debug.Log("cos");
+        }
+        if (Input.GetKeyUp(KeyCode.Y) && klik == true)
+        {
+            anim.SetBool("machaj", false);
+            klik = false;
+            Debug.Log("cdsaos");
+        }
+
+    }
+
+
+
+
+
+    void Open_Dor()
+    {
+        
+    }
+    void Close_Dor()
+    {
+       
+    }
+
+
+}
---
+++ Projekt 6/Assets/SCr/Drzwi.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Numerics;
+using UnityEngine;
+
+public class Drzwi : MonoBehaviour
+{
+    GameObject doors;
+    public  Animator anim;
+
+
+
+
+    void Start()
+    {
+        anim = doors.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    
+    private void OnTriggerExit(Collider col)
+    {
+
+        if (col.gameObject.name == "Player")
+        {
+
+            Close_Dor();
+            
+        }
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+
+            Open_Dor();
+           
+        }
+    }
+    void Open_Dor()
+    {
+        anim.SetBool("IsOp", true);
+    }
+    void Close_Dor()
+    {
+        anim.SetBool("IsOp", false);
+    }
+
+
+}
